c666d0e563625d9ab6fd0d2b4cecb930196db47f
prashantraorane <142421656+prashantraorane@users.noreply.github.com>
2024-01-10 11:25:23 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Maintenance from './Maintenance';
+
+function App() {
+  return (
+    <div className="App">
+      <Maintenance />
+    </div>
+  );
+}
+
+export default App;
